916c911522dc0c1b59a0dedbf9f91f0bae7d06a0
wj <1018584288@qq.com>
2020-09-07 18:07:45 +0800

    zwj

---
+++ 1.c
@@ -0,0 +1,25 @@
+/*===============================================================
+*   Copyright (C) 2020 All rights reserved.
+*   
+*   文件名称：1.c
+*   创 建 者：郑文杰
+*   创建日期：2020年09月07日
+*   描    述：
+*
+*   更新日志：
+*
+================================================================*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
+ int main(int argc, const char *argv[])
+ {
+     
+
+
+     printf ("asdasdasd");
+
+     printf ("123123132");
+     return 0;
+ }
